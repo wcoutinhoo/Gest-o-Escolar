3ca9a251fee7d4f322d76a23e34012756339e658
wcoutinhoo <164575561+wcoutinhoo@users.noreply.github.com>
2024-04-24 20:31:19 -0300

    Add files via upload
    função de adicionar notas

---
+++ Trabaho Front end/assets/js/funçãoadicionarnotas.js
@@ -0,0 +1,10 @@
+function adicionanotaaluno() {
+    let ra=document.getElementById('input_ra').ariaValueMax;
+    let prova1b=parseFloat(document.getElementById('input_prova_1').value);
+    let aep1=parseFloat(document.getElementById('input_aep_1').value);
+    let provaintegrada1b=parseFloat(document.getElementById('input_prova_integrada_1').value);
+    let prova2b=parseFloat (document.getElementById( 'input_prova_2' ).value);
+    let aep2b=parseFloat (document.getElementById('input_aep_2').value);
+    let provaintegrada2b=parseFloat (document.getElementById('input_prova_integrada_2').value);
+
+}
