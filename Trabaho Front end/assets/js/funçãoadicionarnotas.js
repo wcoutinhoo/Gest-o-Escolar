ee6c82602f8d85bf66f4a72b0e56f687776ae47b
wcoutinhoo <164575561+wcoutinhoo@users.noreply.github.com>
2024-04-24 21:07:28 -0300

    Update funçãoadicionarnotas.js

--- Trabaho Front end/assets/js/funçãoadicionarnotas.js
+++ Trabaho Front end/assets/js/funçãoadicionarnotas.js
@@ -1,5 +1,5 @@
 function adicionanota1b() {
-    let ra=document.getElementById('input_ra').ariaValueMax;
+    let ra=document.getElementById('input_ra').value;
     let prova1b=parseFloat(document.getElementById('input_prova_1').value);
     let aep1=parseFloat(document.getElementById('input_aep_1').value);
     let provaintegrada1b=parseFloat(document.getElementById('input_prova_integrada_1').value);
