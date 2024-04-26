aed6eb991dd7d95377c7c1e44104d96bfc9dc356
wcoutinhoo <164575561+wcoutinhoo@users.noreply.github.com>
2024-04-25 21:59:13 -0300

    alguem roubou meu código bando de safado

--- Trabaho Front end/assets/js/app.js
+++ Trabaho Front end/assets/js/app.js
@@ -51,19 +51,3 @@ function media2b(prova2b, aep2, provaintegrada2b){
     const m2 = ((prova2b*0.8)+(aep2*0.1)+(provaintegrada2b*0.1));
     return m2;
 }
-
-
-function mediaFinal(m1, m2){
-    const mediafinal = (Number(m1) + Number(m2)) / 2;
-    return mediafinal;
-}
-
-function determinarsituaçao(mediafinal){
-    if (mediafinal >= 6) {
-        return 'Aprovado';
-    } else if (mediafinal >=3) {
-        return 'Recuperação';
-    } else {
-        return 'Reprovado';
-    }
-}
