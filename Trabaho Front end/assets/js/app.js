da427db30d2fe7b7060b43772d47f4c163f3b661
João Victor <154566658+kkkjjoao@users.noreply.github.com>
2024-04-26 01:55:49 -0300

    ATUALIZEI A FUNÇÃO criaNovoAluno(aluno)

--- Trabaho Front end/assets/js/app.js
+++ Trabaho Front end/assets/js/app.js
@@ -44,17 +44,59 @@ function adicionaDadosAluno() {
 }
 
 
-function criaNovoAluno(nomeAluno, raAluno, emailAluno){
-    const listaAlunos = document.getElementById(listaAlunos);
-    let qtdAlunos = listaAlunos.children.length;
-    const novoItem = document.createElement('li');
-    novoItem.innerText = `Nome: ${aluno['nome']}, Email: ${objeto['email']}, RA: ${objeto.ra}`;
-    novoItem.id = `aluno_id_${qtdAlunos++}`
+function criaNovoAluno(aluno) {
+    const listaAlunos = document.getElementById('tabela_alunos');
+    const novoItem = document.createElement('tr');
 
-    listaAlunos.appendChild(novoItem)
+    const nomeCelula = document.createElement('td');
+    nomeCelula.textContent = aluno.nome;
+    novoItem.appendChild(nomeCelula);
 
-    const aluno = montaObjeto(nomeAluno, raAluno, emailAluno);
-    adicionaLocalStorage(aluno)
+    const raCelula = document.createElement('td');
+    raCelula.textContent = aluno.ra;
+    novoItem.appendChild(raCelula);
+
+    const emailCelula = document.createElement('td');
+    emailCelula.textContent = aluno.email;
+    novoItem.appendChild(emailCelula);
+
+    const aep1Celula = document.createElement('td');
+    aep1Celula.textContent = aluno.aep1;
+    novoItem.appendChild(aep1Celula);
+
+    const integrada1Celula = document.createElement('td');
+    integrada1Celula.textContent = aluno.integrada1;
+    novoItem.appendChild(integrada1Celula);
+
+    const prova1Celula = document.createElement('td');
+    prova1Celula.textContent = aluno.prova1;
+    novoItem.appendChild(prova1Celula);
+
+    const aep2Celula = document.createElement('td');
+    aep2Celula.textContent = aluno.aep2;
+    novoItem.appendChild(aep2Celula);
+
+    const integrada2Celula = document.createElement('td');
+    integrada2Celula.textContent = aluno.integrada2;
+    novoItem.appendChild(integrada2Celula);
+
+    const prova2Celula = document.createElement('td');
+    prova2Celula.textContent = aluno.prova2;
+    novoItem.appendChild(prova2Celula);
+
+    const media1Celula = document.createElement('td');
+    media1Celula.textContent = aluno.media1;
+    novoItem.appendChild(media1Celula);
+
+    const media2Celula = document.createElement('td');
+    media2Celula.textContent = aluno.media2;
+    novoItem.appendChild(media2Celula);
+
+    listaAlunos.appendChild(novoItem);
+
+
+
+    adicionaTabelaLocalStorage(aluno);
 }
 
 function media1b(prova1b, aep1, provaintegrada1b){
